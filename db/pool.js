64252e6275c9265c9291e76e89d649624e48ae70
Noah Gullo <noahgulloingram@gmail.com>
2026-08-01 07:11:25 -0700

    fix: connection url

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,7 @@
-#! .env.development 
+#! .env 
 
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.HOST, 
-  user: process.env.ROLE_NAME,
-  database: "inventory",
-  password: process.env.ROLE_PASSWORD,
-  port: 5432
+  connectionString: process.env.CONNECTION_STRING,
 });
